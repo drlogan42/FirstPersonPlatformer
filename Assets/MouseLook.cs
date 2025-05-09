be15990dc2712965d7062ecb3c9e86bfeaefec7d
Logan <drlogan42@gmail.com>
2025-05-09 15:32:43 -0400

    Created Basic Movement, Camera, Starter Platform

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSens = 100.0f;            //Mouse sensativity
+    public Transform playerBody;                //The position and rotation of the player body
+    float xRotation = 0f;                       //Rotation about x (up-down)
+    public InputActionProperty Look;            //Look is defined for input actions in InputSystem_Actions
+    public InputSystem_Actions playerControls;  //playerControls as the input system actions (assigning keybinds)
+    Vector2 lookDirection = Vector2.zero;       //Empty vector 2 initialized for lookDirection
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;   //Locks cursor in game within window
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();    //Necessary for player input system
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();    //Necessary for player input system
+    }
+    private void Update()
+    {
+        lookDirection = Look.action.ReadValue<Vector2>() * mouseSens * Time.deltaTime;        //lookDirection stores value of Vector2 which holds "horizontal" and "vertical" input (look) from "Look" within InputSystem_Actions
+
+        xRotation -= lookDirection.y;                                                         //Invert Up-Down Axis
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);                                        //Clamp the vertical axis so player cant look upside down
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);                        //Use a quaternion.euler to apply vertical rotation to camera
+        playerBody.Rotate(Vector3.up * lookDirection.x);                                      //Apply horizontal rotation to player body
+
+    }
+}
